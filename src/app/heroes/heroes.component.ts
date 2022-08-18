b9705da15e6a4119a3d38f2dff3ae7fc9b26520b
jaron.schleier <Jaronschleier@gmail.com>
2022-08-18 14:53:40 +0200

    clean up

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {Component} from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -15,10 +15,6 @@ export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero?: Hero;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
